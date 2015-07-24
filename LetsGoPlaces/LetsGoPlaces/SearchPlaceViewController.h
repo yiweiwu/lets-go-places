b766b3e01a12cd677f9af386418ff7efaaff4347
yiwei <yiwei.wu51@gmail.com>
2015-07-23 23:27:26 -0700

    Make SearchPlaceViewController a UITableViewController and add a UISearchController to it

--- LetsGoPlaces/LetsGoPlaces/SearchPlaceViewController.h
+++ LetsGoPlaces/LetsGoPlaces/SearchPlaceViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SearchPlaceViewController : UIViewController
+@interface SearchPlaceViewController : UITableViewController
 
 
 @end
--- LetsGoPlaces/LetsGoPlaces/SearchPlaceViewController.m
+++ LetsGoPlaces/LetsGoPlaces/SearchPlaceViewController.m
@@ -8,21 +8,81 @@
 
 #import "SearchPlaceViewController.h"
 
-@interface SearchPlaceViewController ()
+@interface SearchPlaceViewController () <UISearchBarDelegate, UISearchControllerDelegate, UISearchResultsUpdating>
+
+@property (nonatomic, strong) UISearchController *searchController;
 
 @end
+
+NSString *const placeTableViewCellIdentifier = @"PlaceTableViewCellId";
 
 @implementation SearchPlaceViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.title = @"Let's go places";
     self.view.backgroundColor = [UIColor blueColor];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:placeTableViewCellIdentifier];
+    
+    // Show the search bar
+    [self.searchController.searchBar sizeToFit];
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Properties
+
+- (UISearchController *)searchController
+{
+    if (!_searchController) {
+        _searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+        _searchController.searchResultsUpdater = self;
+        _searchController.delegate = self;
+        _searchController.searchBar.delegate = self;
+    }
+    return _searchController;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:placeTableViewCellIdentifier forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", indexPath];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
+}
+
+#pragma mark - UISearchResultsUpdating
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    // make Google Places API request
 }
 
 @end
